f4e7805c8367ffad2cc456dfceb5fe4fdf53da32
Soundhar Kannan <soundhark@infinite.com>
2025-06-18 16:46:10 +0530

    Commiting first

---
+++ First_Project/First_Project/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace First_Project
+{
+    class Program
+    {
+           
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("write your name : ");*/
+            int name = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(name);
+            Console.Read();
+          
+        }
+    }
+
+
+    
+}
