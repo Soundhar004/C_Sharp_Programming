a6bf3427dc67e2f71884d38cb452cbe800957c42
Soundhar Kannan <soundhark@infinite.com>
2025-06-19 10:59:33 +0530

    check positive

--- First_Project/First_Project/Program.cs
+++ First_Project/First_Project/Program.cs
@@ -10,10 +10,9 @@ namespace First_Project
 
             Program own_class = new Program();
             own_class.CheckEqual();
-            Console.Read();
-         
-
-
+            own_class.CheckPositive();
+            
+            
         }
 
         public void CheckEqual()
@@ -24,11 +23,26 @@ namespace First_Project
             int input_2 = Convert.ToInt32(Console.ReadLine());
             if(input_1 == input_2) {
                 Console.WriteLine($"{input_1} and {input_2} are equal.");
-
             }
             else
             {
-                Console.WriteLine();
+                Console.WriteLine($"{input_1} and {input_2} are not equal.");
+               
+            }
+        }
+
+
+        public void CheckPositive()
+        {
+            Console.WriteLine("Enter Input 1 : ");
+            int input_1 = Convert.ToInt32(Console.ReadLine());
+            if (input_1 > 0)
+            {
+                Console.WriteLine($"{input_1} is a Positive Number.");
+            }
+            else
+            {
+                Console.WriteLine($"{input_1} is a Negative Number");
                 Console.Read();
             }
         }
