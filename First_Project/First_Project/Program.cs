d57cbcff56d315217848fb6fb9956c23ac0900c9
Soundhar Kannan <soundhark@infinite.com>
2025-06-19 10:34:54 +0530

    check equal

--- First_Project/First_Project/Program.cs
+++ First_Project/First_Project/Program.cs
@@ -4,17 +4,33 @@ namespace First_Project
 {
     class Program
     {
-           
-        static void Main(string[] args)
-        {
-            /*Console.WriteLine("write your name : ");*/
-            int name = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(name);
+
+        static void Main(string[] args) {
+
+
+            Program own_class = new Program();
+            own_class.CheckEqual();
             Console.Read();
-          
+         
+
+
+        }
+
+        public void CheckEqual()
+        {
+            Console.WriteLine("Enter Input 1 : ");
+            int input_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Input 2 : ");
+            int input_2 = Convert.ToInt32(Console.ReadLine());
+            if(input_1 == input_2) {
+                Console.WriteLine($"{input_1} and {input_2} are equal.");
+
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.Read();
+            }
         }
     }
-
-
-    
 }
