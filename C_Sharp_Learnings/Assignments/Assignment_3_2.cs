0238be3e25cb3796a71255ce7b1af365e66f6c71
Soundhar Kannan <soundhark@infinite.com>
2025-06-26 11:40:01 +0530

    small update

--- C_Sharp_Learnings/Assignments/Assignment_3_2.cs
+++ C_Sharp_Learnings/Assignments/Assignment_3_2.cs
@@ -8,15 +8,105 @@ namespace C_Sharp_Learnings.Assignments
 {
     class Student
     {
-        
-        public Student(string stud_name, int roll_no, string Class, string branch, int sem)
+        public string stud_name,Class,branch;
+        public int roll_no, sem,avg_marks;
+        bool fail = false;
+       /* public Student(string stud_name, int roll_no, string Class, string branch, int sem)
         {
+            Console.WriteLine(stud_name);
+        }*/
+
+        public void GetInfo(string info)
+        {
+            Console.WriteLine($"Enter the {info} : ");
+            if (info == "name"){
+                this.stud_name = Console.ReadLine();
+            }
+            else if (info == "class"){
+                this.Class = Console.ReadLine();
+            }
+            else if (info == "branch"){
+                this.branch = Console.ReadLine();
+            }
+            else if (info == "roll no"){
+                this.roll_no = Convert.ToInt32(Console.ReadLine());
+            }
+            else if (info == "semester"){
+                this.sem = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        public void GetMarks()
+        {
+            int[] arry = new int[5];
+            for(int i =0; i< arry.Length; i++)
+            {
+                Console.WriteLine("Enter the subject {0} mark ", i + 1);
+                arry[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            this.avg_marks = arry.Sum() / arry.Length;
+
+            foreach(int i in arry)
+            {
+                if (i < 35)
+                {
+                    this.fail = true;
+                }
+                else if (i >=35)
+                {
+                    this.fail = false;
+                }
+            }
+
+
 
         }
 
-
+        public void DisplayInfo()
+        {
+            if(this.fail == false)
+            {
+                if (this.avg_marks > 50)
+                {
+                    Console.WriteLine("Your result is : PASS");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your result is : FAIL");
+            }
+            Console.WriteLine($"Your Average Mark : {this.avg_marks}");
+            Console.WriteLine($"Student name : {this.stud_name}");
+            Console.WriteLine($"Student roll no : {this.roll_no}");
+            Console.WriteLine($"Student class : {this.Class}");
+            Console.WriteLine($"Student branch : {this.branch}");
+            Console.WriteLine($"Student semester : {this.sem}");
+        }
+       
+        
         static void Main()
         {
+           /* Console.WriteLine("Enter the name : ");
+            string stud_name = Console.ReadLine();
+            Console.WriteLine("Enter the roll no : ");*/
+
+           /* string stud_name = Console.ReadLine();
+            Console.WriteLine("Enter the class name : ");
+            string stud_name = Console.ReadLine();
+            Console.WriteLine("Enter the name : ");
+            string stud_name = Console.ReadLine();
+            Console.WriteLine("Enter the name : ");
+            string stud_name = Console.ReadLine();*/
+            Student obj = new Student();
+            obj.GetInfo("name");
+            obj.GetInfo("roll no");
+            obj.GetInfo("class");
+            obj.GetInfo("branch");
+            obj.GetInfo("semester");
+            obj.GetMarks();
+            obj.DisplayInfo();
+            Console.Read();
+
 
         }
 
--- C_Sharp_Learnings/Rough/Rough_1.cs
+++ C_Sharp_Learnings/Rough/Rough_1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,8 +30,8 @@ namespace C_Sharp_Learnings.Rough
     }
 
 
-    /*Question No 2 */
-    /*class Box
+    Question No 2 
+    class Box
     {
         public int tot_len;
         int[] box1, box2;
@@ -49,7 +49,7 @@ namespace C_Sharp_Learnings.Rough
             obj.tot_len = a.box2.Length + b.box1.Length;
             return obj;
             Console.Read();
-            
+
         }
 
         public void Display()
@@ -58,14 +58,15 @@ namespace C_Sharp_Learnings.Rough
 
         }
 
-        static void Main(string[] args){
+        static void Main(string[] args)
+        {
             int[] b1 = { 4, 3 }, b2 = { 4, 3, 2 };
             Box Object = new Box(b1, b2);
             Object.Display();
-            
+
         }
 
-    }*/
+    }
 
     public class Box
     {
@@ -113,5 +114,124 @@ namespace C_Sharp_Learnings.Rough
 
     }
 
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Day7
+    {
+        class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Department { get; set; }
+            public double Salary { get; set; }
+
+            List<Employee> employeeList = new List<Employee>();
+
+            //adding one Employee at a time
+            internal void AddStudent(Employee employee)
+            {
+                employeeList.Add(employee);
+            }
+
+            //Display the student
+
+            internal List<Employee> Displayemployee()
+            {
+                return employeeList;
+            }
+
+            //Get emp by id
+
+            internal List<Employee> GetEmployeebyId(int id)
+            {
+                List<Employee> employee = new List<Employee>();
+
+                foreach (var emp in employeeList)
+                {
+                    if (emp.Id == id)
+                    {
+                        employee.Add(emp);
+
+                    }
+                }
+
+                return employee;
+            }
+
+            public List<Employee> Update_Employee(int id, string name, string department, double salary)
+            {
+                List<Employee> employee = new List<Employee>();
+                foreach (var emp in employeeList)
+                {
+                    if (emp.Id == id)
+                    {
+                        emp.Name = name;
+                        emp.Department = department;
+                        emp.Salary = salary;
+                    }
+                }
+                return employee;
+            }
+
+            public List<Employee> delete_Employee(int id)
+            {
+                List<Employee> employee = new List<Employee>();
+                foreach (var emp in employeeList)
+                {
+                    if (emp.Id == id)
+                    {
+                        employee.Remove(emp);
+                    }
+                }
+                return employee;
+            }
+
+        }
+        class ListDemo
+        {
+            static void Main()
+            {
+                // ArrayList al = new ArrayList();
+                //al.Add(90); //boxing
+
+                List<string> booksList = new List<string>();
+
+                Employee employee = new Employee();
+
+                List<Employee> employees_data = new List<Employee>();
+
+                //Add the student
+
+                employee.AddStudent(new Employee { Id = 1, Name = "Hari", Department = "IT", Salary = 98 });
+                employee.AddStudent(new Employee { Id = 2, Name = "Nila", Department = "IT", Salary = 78 });
+                employee.AddStudent(new Employee { Id = 3, Name = "Suman", Department = "CSE", Salary = 67 });
+                employee.AddStudent(new Employee { Id = 4, Name = "John", Department = "ECE", Salary = 90 });
+
+                //display the student
+
+                studentData = student.DisplayStudent();
+                Console.WriteLine("------------------Display Student---------------------");
+                foreach (var item in studentData)
+                {
+                    Console.WriteLine($"ID:{item.StudentId + " " + item.StudentName + " " + item.StudentCourse }");
+                }
+
+                Console.WriteLine("------------------Display Student by course---------------------");
+                //Display the student by course
+                studentData = student.GetStudentsByCourse("IT");
+                foreach (var item in studentData)
+                {
+                    Console.WriteLine($"ID:{item.StudentId + " " + item.StudentName + " " + item.StudentCourse }");
+                }
+            }
+        }
+    }
+
 
 }
+*/
